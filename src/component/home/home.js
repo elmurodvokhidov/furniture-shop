34b276279d2ac0cdf4f14ddeb0a311efe4167eb5
islom <islomxonahmedov7@gmail.com>
2023-12-09 19:56:13 +0500

    Card

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,11 +1,14 @@
+import Style from "./home.module.css"
 import Section1 from "../section1/section1"
 import Section2 from "../section2/section2"
 import Section3 from "../section3/section3"
 import Section4 from "../section4/section4"
 import Section5 from "../section5/section5"
 import Section6 from "../section6/section6"
-import Section7 from "../section7/section7"
+import Section7, { Card } from "../section7/section7"
 import Section8 from "../section8/section8"
+import rasim1 from "../section7/img/rasim1.png";
+import rasim2 from "../section7/img/rasim2.png";
 
 
 function Home() {
@@ -18,6 +21,10 @@ function Home() {
         <Section5 />
         <Section6 />
         <Section7 />
+        <div className={Style.card}>
+        <Card rasim={rasim1}/>
+        <Card rasim={rasim2}/>
+        </div>
         <Section8 />
         </>
     )
--- src/component/section7/section7.js
+++ src/component/section7/section7.js
@@ -1,7 +1,39 @@
+import section7 from "./section7.module.css";
+import rasim1 from "./img/rasim1.png";
+import rasim2 from "./img/rasim2.png";
 function Section7() {
     return (
-        <div>Section7</div>
+        <div className={section7.container}>
+            <div className={section7.box1}>
+                <h2>What say clients about us</h2>
+                <div className={section7.chiziq}>
+                    <p>_________</p>
+                    <p>It is a long established fact that a reader will <br /> be distracted by the service.</p>
+                </div>
+            </div>
+
+        </div>
     )
 }
-
+export function Card({rasim}) {
+    return (
+        <div className={section7.box2}>
+            <div className={section7.bir}>
+                <img src={rasim} alt="rasim" />
+            </div>
+            <div className={section7.ikki}>
+                <h2>Mr. Jone Ambrose</h2>
+                <p>20 - 07 - 21</p>
+                <p>“Lorem Ipsum has been the industry by standard dummy text ever to since the 1500s, it’s  type and scrambled it to make a type specimen book. It’s has survived not only five.”</p>
+                <div className={section7.i}>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
 export default Section7
