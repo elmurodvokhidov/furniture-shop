20d21ac8257b42d52cb8ce52d6bed6750c83f2d0
Hojiakbar Name <bekmirzayevhojiakbar306@example.com>
2023-12-09 20:03:34 +0500

    section 2 tugadi

--- src/component/section2/section2.js
+++ src/component/section2/section2.js
@@ -1,6 +1,19 @@
+import Style from './section2.module.css'
 function Section2() {
   return (
-    <div>Section2</div>
+    <div className={Style.asos} >
+      <div className={Style.box}>
+        <h1 className={Style.tetx}>Enjoy your life in our <br />
+          luxurious furniture</h1>
+        <h4 className={Style.text2}>If you are looking for a furniture then you have <br />
+          come to the right place.
+        </h4>
+        <button className={Style.button}>Order Now</button>
+
+
+      </div>
+
+    </div>
   )
 }
 
