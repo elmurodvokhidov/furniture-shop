c223bf29e106dadab52bbc4257a3a777817a4de3
islom <islomxonahmedov7@gmail.com>
2023-12-11 13:23:26 +0500

    section 4bilan section3 chaqirilgan joyini almashtirdim

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -16,8 +16,8 @@ function Home() {
         <>
         <Section1 />
         <Section2 />
-        <Section3 />
         <Section4 />
+        <Section3 />
         <Section5 />
         <Section6 />
         <Section7 />
